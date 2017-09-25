1dc067762327a04c085bb4fbeb9b7e9821838969
Shaun Haber <srhaber@gmail.com>
2017-09-24 20:44:59 -0700

    First pass at data bag decryptor

---
+++ data_bag_decryptor.go
@@ -0,0 +1,29 @@
+package chef
+
+type DataBagDecryptor struct {
+	item   map[string]interface{}
+	secret []byte
+}
+
+func (d *DataBagDecryptor) DecryptItem() (DataBagItem, error) {
+	item := map[string]string{
+		"id": d.item["id"].(string),
+	}
+
+	for key, encryptedValue := range d.item {
+		if key == "id" {
+			continue
+		}
+
+		v := NewEncryptedDataBagValue(encryptedValue)
+
+		decryptedValue, err := v.DecryptValue(d.secret)
+		if err != nil {
+			return nil, err
+		}
+
+		item[key] = decryptedValue
+	}
+
+	return item, nil
+}
